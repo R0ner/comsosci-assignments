6ac42ebaa261b688b61b144ce8c1f20b3441b7c8
R0ner <runesehested.rs@gmail.com>
2022-05-03 21:12:06 +0200

    Finished analysis notebook

--- tfidf.py
+++ tfidf.py
@@ -1,7 +1,7 @@
 import nltk
 import numpy as np
 
-def term_frequency(document: list) -> tuple[np.array, np.array]:
+def term_frequency(document: list, normalize: bool = True) -> tuple[np.array, np.array]:
     """This function calculates the term frequency of a document.
     The term frequency is normalized by the total number of words in the document.
     Returns a tuple of arrays. The first array is the terms and the second array is
@@ -13,7 +13,10 @@ def term_frequency(document: list) -> tuple[np.array, np.array]:
     fdist = nltk.FreqDist(document)
     n_words = fdist.N()
     terms = np.array(list(fdist.keys()))
-    tf = np.array(list(fdist.values())) / n_words
+    tf = np.array(list(fdist.values()))
+    
+    if normalize:
+        tf = tf / n_words
     
     return tf, terms
 
@@ -37,9 +40,12 @@ def inverse_document_frequency(documents: dict[list], corpus=None) -> dict[float
     
     return idf_lookup
 
-def tf_idf(document: list, idf_lookup: dict[float]) -> tuple[np.array, np.array]:
+def tf_idf(document: list, idf_lookup: dict[float], return_tf: bool = False) -> tuple[np.array, np.array]:
     """Computes the tf-idf of a document, given a document and a lookup table for idf."""
     tf, terms = term_frequency(document)
     idf = np.array([idf_lookup[term] for term in terms])
     
-    return tf * idf, terms
+    if return_tf:
+        return tf * idf, terms, tf
+    else:
+        return tf * idf, terms    
