a71ea722829be1b6b843b8b0f4caa01bee6ab3a6
R0ner <runesehested.rs@gmail.com>
2022-05-06 18:39:06 +0200

    Final touches

--- src/components/TopicGradeSection/TopicGradeSection.tsx
+++ src/components/TopicGradeSection/TopicGradeSection.tsx
@@ -20,8 +20,17 @@ const TopicGradeSection: FC<TopicGradeSectionProps> = () => (
         <br/>
         <br/>
         The course in question is
-        the one that this project was carried out for and we think topics match the theme. Observe that we are only identifying
+        the one that this project was carried out for and we think that the topics match the theme. Observe that we are only identifying
         single words as topics, so we are not able to differentiate between bigrams such as 'social science' or 'social networks'.
+        <br/>
+        <br/>
+        We have used the topics to visualize the average for all courses in which the topic appears. Use the scrollbar on the figure on 
+        the right hand side to explore the distribution of average grades for every topic that we identified. See if you can find your
+        favorite topic on the list. 
+        <br/>
+        <br/>
+        It seems that certain topics significantly increase the mean average grade of the courses in which they appear in. However, 
+        we leave any conclusive analysis of differences between topics to the reader.
       </div>
       <div className={styles.Scrollable}>
         <div>
